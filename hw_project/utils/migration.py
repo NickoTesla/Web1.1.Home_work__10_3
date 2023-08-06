9ce45931deced855d8e83009439e8bf6f4975285
NickoTesla <mykolandwirt@gmail.com>
2023-08-06 12:18:29 -0600

    update

--- hw_project/utils/migration.py
+++ hw_project/utils/migration.py
@@ -21,3 +21,29 @@ for author in authors:
         description=author['author_description'],
     )
 
+
+quotes = db.quotes.find()
+
+for quote in quotes: 
+    tags = []
+    for tag in quote['tags']:
+      t, *_ = Tag.objects.get_or_create(name=tag)
+      tags.append(t)
+    
+    exist_quote = bool(len(Quote.objects.filter(quote=quote['quote'])))
+
+    if not exist_quote:
+       author = db.authors.find_one({'_id': quote['author']})
+       a = Author.objects.get(fullname=author['full_name'])
+       q = Quote.objects.create(
+          quote=quote['quote'],
+          author=a
+       )
+       for tag in tags:
+          q.tags.add(tag)
+
+
+
+
+
+    
